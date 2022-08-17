967cfdd79474e5e21f1011ddaec01d8539ae0e27
Benio10ebon <110507766+Benio10ebon@users.noreply.github.com>
2022-08-17 16:39:13 -0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -235,7 +235,7 @@ class MainMenuState extends MusicBeatState
 									case 'story_mode':
 										MusicBeatState.switchState(new StoryMenuState());
 									case 'freeplay':
-										MusicBeatState.switchState(new FreeplaySta
+										MusicBeatState.switchState(new FreeplayState());
 									case 'options':
 										LoadingState.loadAndSwitchState(new options.OptionsState());
 								}
